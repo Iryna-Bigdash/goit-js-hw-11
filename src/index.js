a0484fd077080d65c1309831b6f077d037c87804
Iryna B <imbigdash@gmail.com>
2023-04-04 14:45:05 +0100

    added axios
    added asynchrony (async/await)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { renderImgs } from './partials/jsmodules/renderCards';
 
 refs = {
   formEl: document.querySelector('#search-form'),
   cardContainer: document.querySelector('.gallery'),
   loadBtn: document.querySelector('.load-more'),
+  searchBtn: document.querySelector('button[type="submit"]'),
 };
 let searchField = '';
 let currentPage = 1;
@@ -14,75 +17,52 @@ refs.formEl.addEventListener('submit', onFormSubmit);
 refs.loadBtn.addEventListener('click', onloadBtnClick);
 
 refs.loadBtn.classList.add('is-hidden');
-refs.cardContainer.insertAdjacentHTML('beforeend', renderImgs(cards));
 
 const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
   captionDelay: 250,
 });
 
-
-
 function onFormSubmit(e) {
   e.preventDefault();
+  clearGalleryContainer();
   searchField = e.currentTarget.elements.searchQuery.value.trim();
-    resetPage();
-    
-    if (searchField === '') {
-        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-    } else {
-        Notiflix.Notify.success('Hooray! We found totalHits images.'); 
-        getPictures();
-        renderImgs();
-        refs.loadBtn.classList.remove('is-hidden');
-    }
+  resetPage();
+
+  if (searchField === '') {
+    return Notiflix.Notify.failure('Please type the field..');
+  } else {
+    getPictures().then(cards => {
+      if (cards.total === 0) {
+        refs.loadBtn.classList.add('is-hidden');
+        Notiflix.Notify.failure(
+          '"Sorry, there are no images matching your search query. Please try again."'
+        );
+      } else {
+        Notiflix.Notify.success(`Hooray! We found ${cards.totalHits} images.`);
+      }
+    });
+  }
 }
 
-function getPictures() {
+async function getPictures() {
   const myKey = '34703273-719fc97d4df3919ec9a1e2b2b';
-  fetch(
-    `https://pixabay.com/api/?key=${myKey}&q=${searchField}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`
-  )
-    .then(responce => responce.json())
-    .then(data => {
-      currentPage += 1;
-      renderImgs(data);
-    })
-    .catch(console.log('error'));
-}
+  const BASE_URL = 'https://pixabay.com';
+  const URL = `${BASE_URL}/api/?key=${myKey}&q=${searchField}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`;
 
-function renderImgs(cards) {
-return cards.hits.map(
-({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => {
-      return `<div class="photo-card">
-            <a class="photo-link" href="${largeImageURL}">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width ='300px' height ='200px'/><a/>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes: ${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views: ${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments: ${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads: ${downloads}</b>
-    </p>
-  </div>
-</div>`;
-    }
-  )
-  .join('');
+  try {
+    const responce = await axios(URL);
+    const cards = responce.data;
+    refs.cardContainer.insertAdjacentHTML('beforeend', renderImgs(cards));
+    currentPage += 1;
+    refs.loadBtn.classList.remove('is-hidden');
+    lightbox.refresh();
+    return cards;
+  } catch {
+    refs.loadBtn.classList.add('is-hidden');
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 }
 
 function onloadBtnClick() {
@@ -91,4 +71,7 @@ function onloadBtnClick() {
 
 function resetPage() {
   currentPage = 1;
+}
+function clearGalleryContainer() {
+  refs.cardContainer.innerHTML = '';
 }
---
+++ src/partials/jsmodules/renderCards.js
@@ -0,0 +1,36 @@
+export function renderImgs(cards) {
+  return (murkup = cards.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+      <div class="photo-card">
+            <a class="gallery__link" href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width ='300px' height ='200px' class="gallery__image"/>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+  <a/>
+</div>`;
+      }
+    )
+    .join(''));
+}
